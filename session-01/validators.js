a7509b9816297f367c491e069cb8ef29c9873e2f
SHasan59 <hasan.samith@gmail.com>
2024-08-01 21:13:20 -0400

    updated validator

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,22 +7,20 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  if (username.length < 3 || username.length > 10) return false;
+  if (username.length < 3 || username.length > 10){
+     return false;} // 3-10 chars in length
 
-  if (
-    !(username[0] >= "a" && username[0] <= "z") &&
-    !(username[0] >= "A" && username[0] <= "Z")
-  )
-    return false;
+  if (!(username[0] >= "a" && username[0] <= "z") && !(username[0] >= "A" && username[0] <= "Z")){
+    return false;} //must begin with letter
 
   for (let i = 1; i < username.length; i++) {
-    let letter = username[i];
-    if (letter >= "a" && letter <= "z") continue;
-    if (letter >= "A" && letter <= "Z") continue;
-    if (letter >= "0" && letter <= "9") continue;
-    return false;
+    let checker = username[i];
+    if (checker >= "a" && checker <= "z") continue;
+    if (checker >= "A" && checker <= "Z") continue;
+    if (checker >= "0" && checker <= "9") continue;
+    return false; // if it is a special character we return false here
   }
-
+  // else return true
   return true;
 }
 
@@ -33,32 +31,20 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  if (password.length < 10 || password.length > 64) return false;
 
-  let numberCount = 0;
-  for (let i = 0; i < password.length; i++) {
-    let letter = password[i];
-    if (letter >= "0" && letter <= "9") numberCount++;
+  // letter, number, special char sets
+  const containsLetter = /[a-zA-Z]/;
+  const containsNumber = /[0-9]/;
+  const containsSpecial = /[!@#$%^&*()?.,;{}|/\-=]/;
+
+  //len checker
+  if (password.length < 10 || password.length > 64) {
+    return false;
+  } 
+  //checker if 1 of each condition
+  else if (!containsLetter.test(password) || !containsNumber.test(password) || !containsSpecial.test(password)) {
+    return false;
   }
-  if (numberCount === 0) return false;
-
-  let alphaCount = 0;
-  for (let i = 0; i < password.length; i++) {
-    let letter = password[i];
-    if ((letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z"))
-      alphaCount++;
-  }
-  if (alphaCount === 0) return false;
-
-  const specialPrintableCharacters = " !\"#$%&'()*+,-./:;<=>?@{\\]^_`{|}~";
-  let specialCount = 0;
-  for (let i = 0; i < password.length; i++) {
-    let letter = password[i];
-    if (specialPrintableCharacters.includes(letter)) specialCount++;
-  }
-  if (specialCount === 0) return false;
-
   return true;
 }
-
 module.exports = { validUsername, validPassword };
