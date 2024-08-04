bb5346120350009bd091ed14384567f06e5be3a1
Ljupcho-Atanasov <ljatanasus@gmail.com>
2024-08-03 20:47:37 -0400

    Update validators.js

--- session-01/validators.js
+++ session-01/validators.js
@@ -41,9 +41,6 @@ function validPassword(password) {
     else if (isNumber(password[j])) ++countNumber;
         else ++countSpecial;
   }
-
-
-
   return (countLetter > 0) && (countNumber > 0) && (countSpecial > 0) && (password.length >= 10) && (password.length <= 64);
 }
 console.log(validPassword("zeus123&me"));
