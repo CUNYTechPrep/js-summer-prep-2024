1f8b5b8dabf9499c390c040c77aa8228ebe9a91e
minglegao2001 <71712980+minglegao2001@users.noreply.github.com>
2024-08-06 20:57:04 -0400

    Finished validators.js Assignment

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,35 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+
+  let goodUsername = false;
+
+  // username must be between (and including) 3-10 characters in length
+  if(username.length >= 3 && username.length <= 10) {
+
+    // username must begin with a letter 
+    /*
+      /^[a-zA-Z]/ is the regex that test if a expression's first character is a letter
+    */ 
+    if(/^[a-zA-Z]/.test(username)) {
+
+      // username may contain numbers and letters
+      /*
+        /^[A-Za-z0-9]*$/ is the regex that test if a expression contains both number and letter
+      */
+      if (/^[A-Za-z0-9]*$/.test(username)) {
+
+        // username cannot contain special characters
+        /*
+          /[^a-zA-Z0-9]/ is the regex that test if a expression contains special character
+        */
+        if (!(/[^a-zA-Z0-9]/.test(username))) {
+          goodUsername = true; 
+        }
+      }
+    }
+  }
+  return goodUsername;
 }
 
 /*
@@ -17,7 +45,22 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+  let goodPassword = false;
+
+    // password must contain at least 1 letter, 1 number, and 1 special character
+    /*
+      - [A-Za-z\d] Ensures that the first character is an alphabet or digit.
+      - [A-Za-z\d!@#$%^&*()_+]{7,19} will match minimum 7 maximum 19 character. This is required as he presceding character class 
+          would consume a single character making the total number of characters in the string as minimum 8 and maximum 20.
+      - $ Anchors the regex at the end of the string. Ensures that there is nothing following our valid password
+      - / at the beginning and end of regex tells the beginning and end of your regex expression
+    */
+
+    if (/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{9,63}$/.test(password)) {
+      goodPassword = true;  
+    }
+
+  return goodPassword;
 }
 
 module.exports = { validUsername, validPassword };
