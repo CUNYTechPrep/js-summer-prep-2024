7c2c84efaf7888574f84370a44a26ca8cc1c3c68
minglegao2001 <minglegaowork@gmail.com>
2024-08-13 21:09:13 -0400

    finished session-02 assignment

--- session-02/exercise.js
+++ session-02/exercise.js
@@ -3,7 +3,15 @@
   For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
 */
 function transformArrayToUpper(listOfStrings) {
-  return;
+
+  /*
+    - map() method: 使用 map() method 来创建一个 array by applying a function to each element of the array.
+    - word.toUpperCase(): 使用 .toUpperCase() 来转换到大写
+    - Arrow function: The arrow function word => word.toUpperCase() takes each string in the strings array and returns its .toUpperCase version.
+
+  */
+  const upperCase = listOfStrings.map(word => word.toUpperCase());
+  return upperCase;
 }
 
 /*
@@ -16,7 +24,24 @@ function transformArrayToUpper(listOfStrings) {
     the function should return 51
 */
 function sumOfAllAges(listOfStudentObjects) {
-  return;
+  // initializing ageTotal to keep track and add all ages
+  let ageTotal = 0;
+
+  //用 .forEach 来循环所有 element of object Array 里面的 age
+  listOfStudentObjects.forEach(ageFromElement => {
+
+    //用 if condition 来 omit 掉所有不符合的年龄 like empty age or 小于 0岁
+    if (ageFromElement.age >= 0) {
+
+      // 所有符合要求的年龄加到 ageTotal
+      ageTotal += ageFromElement.age;
+    }
+    // else {
+      // console.log("Age is invalid");
+    // }
+    });
+
+  return ageTotal; 
 }
 
 module.exports = { transformArrayToUpper, sumOfAllAges };
