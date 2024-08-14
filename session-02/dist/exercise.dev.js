93fc0e4058747fbe2ca698337914e9381773fc36
Idris Hassan <idrishassan@X.local>
2024-08-13 22:03:09 -0400

    Session 2 Assignment

--- session-02/dist/exercise.dev.js
+++ session-02/dist/exercise.dev.js
@@ -1,10 +1,13 @@
+"use strict";
+
 /*
   Transform the input array of strings into uppercase strings
   For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
 */
 function transformArrayToUpper(listOfStrings) {
- 
-  return listOfStrings.map(string => string.toUpperCase());
+  return listOfStrings.map(function (string) {
+    return string.toUpperCase();
+  });
 }
 /*
   Write a function that returns the sum of all student ages.
@@ -15,10 +18,17 @@ function transformArrayToUpper(listOfStrings) {
     [{ name: 'Sandra', age: 31 }, {}, { name: 'Didi', age: 20}]
     the function should return 51
 */
+
+
 function sumOfAllAges(listOfStudentObjects) {
- return listOfStudentObjects
-    .filter(student => student.age && typeof student.age === 'number')
-    .reduce((accumulator, student) => accumulator + student.age, 0);
+  return listOfStudentObjects.filter(function (student) {
+    return student.age && typeof student.age === 'number';
+  }).reduce(function (accumulator, student) {
+    return accumulator + student.age;
+  }, 0);
 }
 
-module.exports = { transformArrayToUpper, sumOfAllAges };
+module.exports = {
+  transformArrayToUpper: transformArrayToUpper,
+  sumOfAllAges: sumOfAllAges
+};
